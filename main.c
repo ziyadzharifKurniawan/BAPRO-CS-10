2e1ccbca8aba824d68b147c4609d514ebbc11eb6
Christian Hadiwijaya <111947738+Tianrider@users.noreply.github.com>
2024-11-28 03:36:16 +0700

    Update main.c

--- main.c
+++ main.c
@@ -14,8 +14,8 @@ void decrypt(char *message, char *key) {
 }
 
 int main() {
-    char message[] = "0X`RLmhiQH.VT1b[F"; // Message to encrypt
-    char *key = "digilsb";                // Encryption key
+    char message[] = "0?13j#s^kd!ncji0"; // Message to encrypt
+    char key[] = "digileb";              // Encryption key
 
     // Decrypt the message
     decrypt(message, key);
