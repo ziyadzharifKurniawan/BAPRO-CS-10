ee69c861c61ea2455d7a0dfa3e5e874a4d7b9bce
Tianrider <hadiwijayachristian7@gmail.com>
2024-11-28 03:33:01 +0700

    Initial Commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MIN_ASCII 32
+#define MAX_ASCII 126
+#define RANGE (MAX_ASCII - MIN_ASCII + 1)
+
+// Function to decrypt the message
+void decrypt(char *message, char *key) {
+    int keyLength = strlen(key);
+    for (int i = 0; message[i] != '\0'; i++) {
+        message[i] = MIN_ASCII + ((message[i] - MIN_ASCII - (key[i % keyLength] - MIN_ASCII) + RANGE) % RANGE);
+    }
+}
+
+int main() {
+    char message[] = "0X`RLmhiQH.VT1b[F"; // Message to encrypt
+    char *key = "digilsb";                // Encryption key
+
+    // Decrypt the message
+    decrypt(message, key);
+    printf("Decrypted message: %s\n", message);
+
+    return 0;
+}
