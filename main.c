6c634fc80ea99aaf3f72bea6e2686996d124060b
ziyadzharifKurniawan <ziyadkurniawan1@gmail.com>
2024-11-28 11:12:23 +0700

    commiting your life

--- main.c
+++ main.c
@@ -14,8 +14,9 @@ void decrypt(char *message, char *key) {
 }
 
 int main() {
-    char message[] = "0?13j#s^kd!ncji0"; // Message to encrypt
-    char key[] = "temporary_key";              // Encryption key
+     char message[] = "0X`RLmhiQH.VT1b[F"; // Message to encrypt
+	 char *key = "temporary_key";                // Encryption key
+
 
     // Decrypt the message
     decrypt(message, key);
